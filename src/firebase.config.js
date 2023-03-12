f00f5d7a7b97a8f396548f4f3438719d933fbadd
Safiyah <safiyahmasud@gmail.com>
2023-03-12 22:50:37 +0100

    nav and routes

---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyDqXOqoV1UtyhULbpnv7xOo90GvoWG-EkQ',
+	authDomain: 'house-marketplace-9be32.firebaseapp.com',
+	projectId: 'house-marketplace-9be32',
+	storageBucket: 'house-marketplace-9be32.appspot.com',
+	messagingSenderId: '851949722431',
+	appId: '1:851949722431:web:1695634b42c49ca663518a',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore()
